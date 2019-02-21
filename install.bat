819542c40f66546e0bee5230420a8b59f17b9867
mathatan <mathatan@gmail.com>
2019-02-21 10:59:17 +0200

    Update install.bat
    Update repository name

--- install.bat
+++ install.bat
@@ -1,8 +1,8 @@
 @echo off
 :: To install 
-:: $ powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/mathatan/gVimRC/master/install.bat -OutFile install.bat" && install.bat
+:: $ powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/mathatan/dotfiles/master/install.bat -OutFile install.bat" && install.bat
 
-powershell -ExecutionPolicy Bypass -Command "Invoke-WebRequest https://raw.githubusercontent.com/mathatan/gVimRC/master/admin-parts.ps1 -OutFile admin.ps1;.\admin.ps1"
+powershell -ExecutionPolicy Bypass -Command "Invoke-WebRequest https://raw.githubusercontent.com/mathatan/dotfiles/master/admin-parts.ps1 -OutFile admin.ps1;.\admin.ps1"
 echo Waiting for installation.
 pause
 :: Get System PATH
@@ -15,11 +15,11 @@ for /f "tokens=3*" %%A in ('reg query "HKCU\Environment" /v Path') do set userpa
 set PATH=%path%;%userpath%;%syspath%
 
 cd %USERPROFILE%
-git clone https://github.com/mathatan/gVimRC.git
-git clone https://github.com/VundleVim/Vundle.vim.git %USERPROFILE%/gVimRC/vim/bundle/Vundle.vim
-runas /user:administrator "cmd.exe /C cd %userprofile% & mklink /d .vim gVimRC\vim & mklink _vimrc gVimRC\_vimrc"
+git clone https://github.com/mathatan/dotfiles.git
+git clone https://github.com/VundleVim/Vundle.vim.git %USERPROFILE%/dotfiles/vim/bundle/Vundle.vim
+runas /user:administrator "cmd.exe /C cd %userprofile% & mklink /d .vim dotfiles\vim & mklink _vimrc dotfiles\_vimrc"
 :: Figure out how to make fonts work
-::powershell -Command "$fonts = (New-Object -ComObject Shell.Application).Namespace(0x14); Get-ChildItem -Recurse -include gVimRC/fonts/*.ttf | % { $fonts.CopyHere($_.fullname) }"
+::powershell -Command "$fonts = (New-Object -ComObject Shell.Application).Namespace(0x14); Get-ChildItem -Recurse -include dotfiles/fonts/*.ttf | % { $fonts.CopyHere($_.fullname) }"
 cmd /c vim +PluginInstall +qall
 cmd /c cyg-get.bat rsync-3.1.2-1 openssh-7.5p1-1 zsh-5.1.1-1
 
