bd369f6c2a93daed234f69d672595aa75fcb54af
Markus Haverinen <mathatan@gmail.com>
2019-03-03 22:59:55 +0200

    Fixes to setup_android.sh

--- setup_android_sdk.sh
+++ setup_android_sdk.sh
@@ -1,17 +1,18 @@
+#/bin/bash
 # Orignally from https://gist.github.com/jjvillavicencio/18feb09f0e93e017a861678bc638dcb0
 
 cd ~
 sudo apt-get install unzip
-wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
-unzip sdk-tools-linux-4333796.zip -d Android
-rm sdk-tools-linux-4333796.zip
+#wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
+#unzip sdk-tools-linux-4333796.zip -d Android
+#rm sdk-tools-linux-4333796.zip
 sudo apt-get install -y lib32z1 openjdk-8-jdk
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export PATH=$PATH:$JAVA_HOME/bin
-printf "\n\nexport JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64\nexport PATH=\$PATH:\$JAVA_HOME/bin" >> ~/.zsh.after/env
+export PATH="$PATH:$JAVA_HOME/bin"
+printf "\n\nexport JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64\nexport PATH=\"\$PATH:\$JAVA_HOME/bin\"" >> ~/.zsh.after/env
 cd Android/tools/bin
 ./sdkmanager "platform-tools" "platforms;android-26" "build-tools;26.0.3"
-export ANDROID_HOME=/home/<user>/Android
+export ANDROID_HOME=$HOME/Android
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 printf "\n\nexport ANDROID_HOME=/home/<user>/Android\nexport PATH=\$PATH:\$ANDROID_HOME/tools\nexport PATH=\$PATH:\$ANDROID_HOME/platform-tools" >> ~/.zsh.after/env
